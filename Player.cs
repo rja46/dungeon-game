3ee7634e6016f0223a5fed01a2431bff89634ed5
rja46 <119348334+rja46@users.noreply.github.com>
2023-01-19 20:08:28 +0000

    finished player

--- Player.cs
+++ Player.cs
@@ -13,21 +13,41 @@ internal class Player
     private string[] classNames = {"Warrior", "Assassin", "Mage"};
     public Player()
     {
+        baseHealth = rng.Next(1, 21);
+        baseStrength = rng.Next(1, 21);
+        baseSpeed = rng.Next(1, 21);
+        baseDex = rng.Next(1, 21);
         System.Console.WriteLine("Choose a class:");
         System.Console.WriteLine("[1] Warrior");
         System.Console.WriteLine("[2] Assassin");
         System.Console.WriteLine("[3] Mage");
-        System.Console.Write("Enter your choice: ");
+        System.Console.Write("\nEnter your choice: ");
         classID = Convert.ToInt32(Console.ReadLine());
         classID--;
+        if (classID == 0)
+        {
+            classHealth = rng.Next(1, 6);
+            classStrength = rng.Next(1, 6);
+        }
+        else if (classID == 1)
+        {
+            classSpeed = rng.Next(1, 6);
+            classDex = rng.Next(1, 6);
+        }
+        else if (classID == 2)
+        {
+            classHealth = rng.Next(1, 6);
+            classDex = rng.Next(1, 6);
+        }
+        Console.Clear();
         display_character();
     }
     public void display_character()
     {
-        System.Console.WriteLine("Class: " + classNames[classID]);
-        System.Console.WriteLine("Health: " + baseHealth);
-        System.Console.WriteLine("Strength: " + baseStrength);
-        System.Console.WriteLine("Speed: " + baseSpeed);
-        System.Console.WriteLine("Dex: " + baseDex);
+        System.Console.WriteLine("Class:    " + classNames[classID]);
+        System.Console.WriteLine("Health:   " + baseHealth.ToString().PadLeft(2, '0') + " + " + classHealth.ToString().PadLeft(2, '0'));
+        System.Console.WriteLine("Strength: " + baseStrength.ToString().PadLeft(2, '0') + " + " + classStrength.ToString().PadLeft(2, '0'));
+        System.Console.WriteLine("Speed:    " + baseSpeed.ToString().PadLeft(2, '0') + " + " + classSpeed.ToString().PadLeft(2, '0'));
+        System.Console.WriteLine("Dex:      " + baseDex.ToString().PadLeft(2, '0') + " + " + classDex.ToString().PadLeft(2, '0'));
     }
 }
