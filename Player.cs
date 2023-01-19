e2720d4b06aee9095457996e1831b7ca02203522
rja46 <119348334+rja46@users.noreply.github.com>
2023-01-19 14:52:39 +0000

    started display

--- Player.cs
+++ Player.cs
@@ -1,10 +1,14 @@
 internal class Player
 {
     Random rng = new Random();
-     private int health;
-    private int strength;
-    private int speed;
-    private int dexterity;
+    private int baseHealth = rng.next(1,21);
+    private int classHealth;
+    private int baseStrength = rng.next(1,21);
+    private int classStrength;
+    private int baseSpeed = rng.next(1,21);
+    private int classSpeed;
+    private int baseDex = rng.next(1,21);
+    private int classDex;
     private int classID;
     private string[] classNames = {"Warrior", "Assassin", "Mage"};
     public Player()
@@ -18,8 +22,9 @@ internal class Player
         classID--;
         display_class();
     }
-    public void display_class()
+    public void display_character()
     {
-        System.Console.WriteLine(classNames[classID]);
+        System.Console.WriteLine("Class: " + classNames[classID]);
+        System.Console.WriteLine("\n Health: {0}\n Strength: {1}");
     }
 }
